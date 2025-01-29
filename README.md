b6550651164cac2a4659c07625bfe0e9d690e88c
ag1O1 <amrashraf20210@gmail.com>
2025-01-29 12:49:37 +0200

    fix

---
+++ README.md
@@ -0,0 +1,23 @@
+# Discord Ollama bot
+
+- written in rust
+
+- this bot is still very early in development so keep that in mind, currently
+  not intended for other people to use and is only for my own specific discord
+  bot, eventually i will refactor the code, clean it up and will be available
+  for other people to use
+
+note: currently requires an environment variable for the bot token, I am using a
+nix shell to achieve this for now.
+
+## features:
+
+- [ ] basic functionallity and settings (get to a state where i can replace this
+      with a previous one i made in python)
+- [ ] add nix development shell (for those 0.001% of nix users who might use
+      this)
+- [ ] save system using json
+- [ ] unhardcode a bunch of stuff (add customizability)
+- [ ] refactor
+- [ ] add option to change bot settings purely via discord commands for
+      convinience
--- flake.nix
+++ flake.nix
@@ -9,21 +9,35 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
+      # some dependencies, OPENSSL is required for the ollama rust lib.
+      # i might be forgetting some dependency that i already have installed on my system, if you find one open a pr adding it.
+      # also i don't remember what gcc is for ¯\_(ツ)_/¯
       nativeBuildInputs = with pkgs; [
         gcc
         pkg-config
+        cargo
+        rustc
       ];
       buildInputs = with pkgs; [
         openssl
       ];
+
+      ### required env vars for OPENSSL to work properly under nixos.
       OPENSSL_DIR = "${pkgs.openssl.dev}";
       PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
       OPENSSL_NO_VENDOR = 1;
       OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
+      # bot token
       DISCORD_TOKEN = "<token>";
       shellHook = ''
         fish
       '';
     };
-  };
+    # i use this to run the discord bot.
+    devShells.x86_64-linux.run = pkgs.mkShell {
+      DISCORD_TOKEN = "<discord_token>";
+      shellHook = ''
+        /home/amr/projects/rust/discord-ollama/target/debug/discord-ollama
+      '';
+    };
 }
